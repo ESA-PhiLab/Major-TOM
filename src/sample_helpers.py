26b157fb8c236bf7fa7ceb3d6fa92f802d89c674
mikonvergence <mikolaj.czerkawski@esa.int>
2024-03-08 10:39:19 +0100

    Update sample_helpers.py

--- src/sample_helpers.py
+++ src/sample_helpers.py
@@ -1,6 +1,7 @@
 from rasterio.io import MemoryFile
 import matplotlib.pyplot as plt
 import numpy as np
+from PIL import Image
 
 def plot(sample, bands = ['B04', 'B03', 'B02'], scaling=2e3):
     img = []
@@ -12,3 +13,7 @@ def read_tif_bytes(tif_bytes):
     with MemoryFile(tif_bytes) as mem_f:
         with mem_f.open(driver='GTiff') as f:
             return f.read().squeeze()
+
+def read_png_bytes(png_bytes):
+    stream = BytesIO(png_bytes)
+    return Image.open(stream)
